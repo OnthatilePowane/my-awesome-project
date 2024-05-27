dfd71f71221110a14f05b6cdf4877d0fede7b448
OnthatilePowane <thationthatile95@gmail.com>
2024-05-28 01:52:56 +0200

    console logged the data to check where my description is

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
+
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
+  console.log(response.data);
 }
 
 function search(event) {
