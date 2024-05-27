54f5b617167a96003eab7b22c4afdd539688d674
OnthatilePowane <thationthatile95@gmail.com>
2024-05-28 01:58:34 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
-
-  cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = temperature;
   console.log(response.data);
 }
+cityElement.innerHTML = response.data.city;
+temperatureElement.innerHTML = temperature;
 
 function search(event) {
   event.preventDefault();
